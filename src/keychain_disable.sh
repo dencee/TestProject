dcfcaa4eb8020fcba5272f666060b69eadabcc01
Daniel-Commins <50278797+Daniel-Commins@users.noreply.github.com>
2019-06-04 20:49:25 -0700

    Verified keychain disable script

--- src/keychain_disable.sh
+++ src/keychain_disable.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# As Administrator, run in a terminal using: sudo bash keychain_disable.sh 
+
 echo -ne '\n' | git credential-osxkeychain erase host=github.com protocol=https
 printf "protocol=https\nhost=github.com\n" | git credential-osxkeychain erase
 
@@ -8,8 +10,7 @@ git config --system --unset credential.helper
 
 keychainDisabled=$(git config --system credential.helper)
 
-if[ keychainDisabled = osxkeychain ]
-then
+if [ "$keychainDisabled" == "osxkeychain" ]; then
   echo "Keychain NOT disabled"
 else
   echo "Keychain disabled"
