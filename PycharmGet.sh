78717b4fb38d738938b4eb8c36dfa27694199611
Daniel Commins <dencee@users.noreply.github.com>
2020-08-03 15:10:47 +0000

    pycharm install script

---
+++ PycharmGet.sh
@@ -0,0 +1,4 @@
+wget https://download.jetbrains.com/python/pycharm-community-2020.2.tar.gz
+tar xfz pycharm-community-2020.2.tar.gz 
+rm pycharm-community-2020.2.tar.gz 
+./pycharm-community-2020.2/bin/pycharm.sh
