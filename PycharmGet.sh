025b45053cd736a666a159dd0218128bdeb9da34
Daniel Commins <dencee@users.noreply.github.com>
2020-08-03 08:39:02 -0700

    Update PycharmGet.sh

--- PycharmGet.sh
+++ PycharmGet.sh
@@ -2,4 +2,4 @@ cd /headless
 wget https://download.jetbrains.com/python/pycharm-community-2020.2.tar.gz
 tar xfz pycharm-community-2020.2.tar.gz 
 rm pycharm-community-2020.2.tar.gz 
-./pycharm-community-2020.2/bin/pycharm.sh
+sudo ./pycharm-community-2020.2/bin/pycharm.sh
